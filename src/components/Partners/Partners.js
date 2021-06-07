19efc9362112ba5fbda3de2f573ec765df38fbf9
anamikaprksh <anmkprksh@gmail.com>
2021-06-08 01:14:13 +0530

    bgpic added

--- src/components/Partners/Partners.js
+++ src/components/Partners/Partners.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import clsx from "clsx";
+import "./Partners.css";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import handleOpen from "./Partnersopen";
@@ -8,7 +9,7 @@ import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import { FlareSharp } from "@material-ui/icons";
-
+import "./Partners.css";
 const useStyles = makeStyles((theme) => ({
   root: {
     "& > *": {
@@ -53,12 +54,13 @@ export default function Partners(props) {
   const [openadd,setOpenadd]=useState(false)
   console.log(openadd)
   return (
-    <div className={classes.root}>
+    <div className={classes.root} >
       {/* <div  className={clsx( {
           // eslint-disable-next-line no-restricted-globals
           // [classes.ShiftLeft]:!open,
           [classes.ShiftRight]: true,
         })}> */}
+        <div className="bgpic">
       <Grid container spacing={3}>
         <Grid item xs={12} sm={6} md={6}>    
             {" "}
@@ -86,6 +88,7 @@ export default function Partners(props) {
         </Grid>
         
       </Grid>
+      </div>
     </div>
   );
 }
